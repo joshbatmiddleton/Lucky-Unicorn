75fd618a086e88d17fa1b5f196131dce2949ad5f
joshbatmiddleton <98915670+joshbatmiddleton@users.noreply.github.com>
2022-03-31 09:35:27 +1300

    Update v1.0

---
+++ 01_Yes_No_Checker/01_Yes_No_Checker_v1.py
@@ -0,0 +1,8 @@
+# Ask the user if they have played before:
+
+show_instructions = input("Have you played Lucky Unicorn before? ")
+
+# If they say yes, output "Program continues"
+
+if show_instructions == "yes":
+    print("")
