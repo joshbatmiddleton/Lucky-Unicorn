4a33b9f21d723d4da7ec36919eb218664d907807
joshbatmiddleton <98915670+joshbatmiddleton@users.noreply.github.com>
2022-04-14 09:36:30 +1200

    Update v1.1.1
    Update v1.1.1-beta 1
    
    Update v1.1.1-beta 2

---
+++ MainProject/03_Instructions/03_Instructions_v1.py
@@ -0,0 +1,46 @@
+# Took the code from 02_Yes_No_Function and made it the base for the instructions,
+# and also incorporated new things, such as instructions.
+
+
+# functions go here:
+def yes_no(question_text):
+    while True:
+
+        # Ask the user if they have played before:
+        answer = input(question_text).lower()
+
+        # If they say yes, output "Program continues"
+        if answer == "yes" or answer == "Yes" or answer == "Y" or answer == "y":
+            answer = "Yes"
+            return answer
+
+        # If they say no, output "display instructions"
+        elif answer == "no" or answer == "No" or answer == "N" or answer == "n":
+            answer = "No"
+            return answer
+
+        # Otherwise, show error message
+        else:
+            print("Please answer 'yes' or 'no'.")
+
+# function to display instructions
+
+
+def instructions():
+    print("**** How To Play ****")
+    print()
+    print("The rules will go here")
+    print()
+    print("Program Continues")
+    print()
+
+
+# Main Routine goes here.
+played_before = yes_no("Have you played Lucky Unicorn Before? ")
+print(f"You entered '{played_before}'")
+print()
+
+if played_before == "No":
+    instructions()
+else:
+    print("Program Continues")
