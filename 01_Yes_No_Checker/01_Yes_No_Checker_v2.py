b3b85c14c70167a0b631b075cc796f919d65548c
joshbatmiddleton <98915670+joshbatmiddleton@users.noreply.github.com>
2022-05-09 18:36:54 +1200

    Pre_Release 1
    commit 1
    
    commit
    
    Initial commit
    
    Update v1.0
    
    Update v1.0
    
    Update v1.0.1
    
    Update v1.0.2
    
    Update v1.0.3
    
    Update v1.1
    
    Update v1.1.1
    
    Update v1.0.1

---
+++ 01_Yes_No_Checker/01_Yes_No_Checker_v2.py
@@ -0,0 +1,18 @@
+# Ask the user if they have played before:
+
+show_instructions = input("Have you played Lucky Unicorn before? ").lower()
+
+# If they say yes, output "Program continues"
+
+if show_instructions == "yes" or show_instructions == "Yes" or show_instructions == "Y" or show_instructions == "y":
+    print("Program Continues")
+
+# If they say no, output "display instructions"
+elif show_instructions == "no" or show_instructions == "No" or show_instructions == "N" or show_instructions == "n":
+    print("Display Instructions")
+
+# Otherwise, show error message
+else:
+    print("Please answer 'yes' or 'no'.")
+
+print(f"You entered '{show_instructions}'")
